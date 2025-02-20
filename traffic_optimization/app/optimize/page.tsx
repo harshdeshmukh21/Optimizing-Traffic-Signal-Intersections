b821af52cf1409833c1aa63e0b00b645c3a0a7ce
harshdeshmukh21 <deshmukhharsh2122@ternaengg.ac.in>
2025-02-21 00:24:44 +0530

    created a next project, added shadcn and created a dashboard layout

---
+++ traffic_optimization/app/optimize/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React from "react";
+import { FileUpload } from "@/components/ui/file-upload";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+const page = () => {
+  return (
+    <div className="">
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </header>
+          <div className="flex items-center justify-center h-full">
+            <FileUpload></FileUpload>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
+};
+
+export default page;
